a5177d72375dbac295dcb98a69b915362673df7f
Tomasz Tkocz <tkoczt@gmail.com>
2022-06-15 18:00:27 +0200

    Initial commit

---
+++ Tkocz_KalkulatorBMI_ProjektowanieUI/calculatorBMI.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tkocz_KalkulatorBMI_ProjektowanieUI
+{
+    /// <summary>
+    /// Interaction logic for calculatorBMI.xaml
+    /// </summary>
+    public partial class calculatorBMI : Window
+    {
+        public calculatorBMI()
+        {
+            InitializeComponent();
+        }
+    }
+}
